e526b882728d26600a5af841a525e176e8e5f28b
pcarette <pierre.carette78@gmail.com>
2024-10-29 17:41:37 +0100

    core: add debt token contracts for central bank module

---
+++ src/citadel-tokens/debt/DebtToken.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.9;
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {IDebtToken} from './interfaces/IDebtToken.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {ReentrancyGuard} from '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import {StandardAccessControlEnumerable} from '../../roles/StandardAccessControlEnumerable.sol';
+import {ERC20Permit} from '@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
+
+contract DebtToken is
+  IDebtToken,
+  ReentrancyGuard,
+  StandardAccessControlEnumerable,
+  ERC20Permit
+{
+  using SafeERC20 for IERC20;
+
+  IERC20 private immutable jFiat;
+  uint256 private immutable capAmount;
+
+  uint256 private donatedAmount;
+  uint256 private bondedAmount;
+  uint256 private withdrawnAmount;
+
+  constructor(
+    IERC20 _jFiat,
+    uint256 _capAmount,
+    string memory _tokenName,
+    string memory _tokenSymbol,
+    Roles memory _roles
+  ) ERC20Permit(_tokenName) ERC20(_tokenName, _tokenSymbol) {
+    jFiat = _jFiat;
+    require(_capAmount > 0, 'No cap set');
+    capAmount = _capAmount;
+    _setAdmin(_roles.admin);
+    _setMaintainer(_roles.maintainer);
+  }
+
+  /**
+   * @notice Deposit j-asset in the debt token contract
+   * @param _amount Amount of j-asset to deposit
+   * @param _isDonation If true no debt-tokens are minted, if false 1:1
+   */
+  function depositJFiat(uint256 _amount, bool _isDonation)
+    external
+    nonReentrant
+  {
+    require(
+      jFiat.balanceOf(address(this)) + withdrawnAmount + _amount <= capAmount,
+      'Cap supply reached'
+    );
+    // pull user jFiat into this contract
+    jFiat.safeTransferFrom(msg.sender, address(this), _amount);
+
+    if (_isDonation) {
+      donatedAmount += _amount;
+      emit Donated(msg.sender, _amount);
+    } else {
+      bondedAmount += _amount;
+
+      // mint debt token to user
+      _mint(msg.sender, _amount);
+      emit Bonded(msg.sender, _amount);
+    }
+  }
+
+  /**
+   * @notice Allow maintainer to withdraw jFiat into a recipient
+   * @notice Only maintainer can call this function
+   * @param _amount Amount of j-asset to withdraw
+   * @param _recipient Address will receive j-asset
+   */
+  function withdrawJFiat(uint256 _amount, address _recipient)
+    external
+    onlyMaintainer
+    nonReentrant
+  {
+    jFiat.transfer(_recipient, _amount);
+    withdrawnAmount += _amount;
+    emit Withdrawn(_amount, _recipient);
+  }
+
+  /**
+   * @notice Returns address of the synthetic token associated to the debt token
+   * @return Address of the synthetic token
+   */
+  function jAsset() external view returns (address) {
+    return address(jFiat);
+  }
+
+  /**
+   * @notice Returns the max amount of donated and bonded j-asset
+   * @return Max balance of donated and bonded j-asset
+   */
+  function cap() external view returns (uint256) {
+    return capAmount;
+  }
+
+  /**
+   * @notice Returns balance of the synthetic token holded by the debt-token
+   * @return Balance of the synthetic token holded
+   */
+  function jFiatBalance() external view returns (uint256) {
+    return jFiat.balanceOf(address(this));
+  }
+
+  /**
+   * @notice Returns balance of the synthetic token donated by users
+   * @return Balance of the synthetic token donated
+   */
+  function donated() external view returns (uint256) {
+    return jFiat.balanceOf(address(this)) + withdrawnAmount - bondedAmount;
+  }
+
+  /**
+   * @notice Returns balance of the synthetic token bonded by users
+   * @return Balance of the synthetic token bonded
+   */
+  function bonded() external view returns (uint256) {
+    return bondedAmount;
+  }
+
+  /**
+   * @notice Returns balance of the synthetic token withdrawn by the maintainer
+   * @return Balance of the synthetic token withdrawn
+   */
+  function withdrawn() external view returns (uint256) {
+    return withdrawnAmount;
+  }
+}
---
+++ src/citadel-tokens/debt/DebtTokenFactory.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.9;
+
+import {IStandardERC20} from '../../base/interfaces/IStandardERC20.sol';
+import {EnumerableSet} from '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
+import {StringUtils} from '../../base/utils/StringUtils.sol';
+import {DebtToken} from './DebtToken.sol';
+import {ReentrancyGuard} from '@openzeppelin/contracts/security/ReentrancyGuard.sol';
+import {StandardAccessControlEnumerable} from '../../roles/StandardAccessControlEnumerable.sol';
+
+contract DebtTokenFactory is ReentrancyGuard, StandardAccessControlEnumerable {
+  using EnumerableSet for EnumerableSet.Bytes32Set;
+  using StringUtils for string;
+  using StringUtils for bytes32;
+
+  mapping(string => address) private debtTokens;
+
+  EnumerableSet.Bytes32Set private syntheticTokens;
+
+  event DebtTokenCreated(address indexed jAsset, address indexed debtToken);
+
+  constructor(Roles memory _roles) {
+    _setAdmin(_roles.admin);
+    _setMaintainer(_roles.maintainer);
+  }
+
+  /**
+   * @notice Create a debt-token associated to synthetic asset
+   * @param _jFiat Synthetic asset
+   * @param _capAmount Max balance of donated and bonded synthetic asset
+   * @param _tokenName Name of the debt-token
+   * @param _tokenSymbol Symbol of the debt-token
+   * @param _roles Admin and maintainer roles
+   * @return debtTokenContract Address of the debt-token deployed
+   */
+  function createDebtToken(
+    IStandardERC20 _jFiat,
+    uint256 _capAmount,
+    string memory _tokenName,
+    string memory _tokenSymbol,
+    Roles memory _roles
+  ) external onlyMaintainer nonReentrant returns (DebtToken debtTokenContract) {
+    string memory symbol = _jFiat.symbol();
+    require(
+      syntheticTokens.add(symbol.stringToBytes32()),
+      'Debt token already created'
+    );
+    debtTokenContract = new DebtToken(
+      _jFiat,
+      _capAmount,
+      _tokenName,
+      _tokenSymbol,
+      _roles
+    );
+    address debtTokenAddr = address(debtTokenContract);
+
+    debtTokens[symbol] = debtTokenAddr;
+
+    emit DebtTokenCreated(address(_jFiat), debtTokenAddr);
+  }
+
+  /**
+   * @notice Returns the address of the debt-token associated to a synthetic asset
+   * @param _tokenSymbol Synthetic asset symbol
+   * @return Address of the debt-token
+   */
+  function debtToken(string calldata _tokenSymbol)
+    external
+    view
+    returns (address)
+  {
+    address debtTokenAddr = debtTokens[_tokenSymbol];
+    require(debtTokenAddr != address(0), 'Debt token not supported');
+    return debtTokenAddr;
+  }
+
+  /**
+   * @notice Returns all the synthetic token symbol used
+   * @return List of all synthetic token symbol
+   */
+  function getSyntheticTokens() external view returns (string[] memory) {
+    uint256 numberOfSynthTokens = syntheticTokens.length();
+    string[] memory synthTokens = new string[](numberOfSynthTokens);
+    for (uint256 j = 0; j < numberOfSynthTokens; j++) {
+      synthTokens[j] = syntheticTokens.at(j).bytes32ToString();
+    }
+    return synthTokens;
+  }
+}
